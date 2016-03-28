34194fb7c46b2513a70d1df18eb6302363c3bbdf
olebedev <ole6edev@gmail.com>
2016-03-28 17:16:14 +0500

    Add Funcs method and keep template state

--- template.go
+++ template.go
@@ -11,48 +11,61 @@ type AssetDirFunc func(string) ([]string, error)
 type BinTemplate struct {
 	Asset    AssetFunc
 	AssetDir AssetDirFunc
+	fm       map[string]interface{}
+	tmpl     *template.Template
 }
 
 func New(a AssetFunc, b AssetDirFunc) *BinTemplate {
-	return &BinTemplate{Asset: a, AssetDir: b}
+	return &BinTemplate{
+		Asset:    a,
+		AssetDir: b,
+		fm:       make(map[string]interface{}),
+		tmpl:     template.New(""),
+	}
 }
 
-func (t *BinTemplate) LoadDirectory(directory string) (*template.Template, error) {
-	var tmpl *template.Template
+func (t *BinTemplate) Funcs(fm map[string]interface{}) *BinTemplate {
+	t.fm = fm
+	t.tmpl = t.tmpl.Funcs(t.fm)
+	return t
+}
+
+func (t *BinTemplate) Template() *template.Template {
+	if t.tmpl == nil {
+		t.tmpl = &template.Template{}
+	}
+	return t.tmpl
+}
+
+func (t *BinTemplate) LoadDirectory(directory string) error {
 
 	files, err := t.AssetDir(directory)
 	if err != nil {
-		return tmpl, err
+		return err
 	}
 
 	for _, filePath := range files {
 		contents, err := t.Asset(directory + "/" + filePath)
 		if err != nil {
-			return tmpl, err
+			return err
 		}
 
 		name := filepath.Base(filePath)
 
-		if tmpl == nil {
-			tmpl = template.New(name)
+		if name != t.tmpl.Name() {
+			t.tmpl = t.tmpl.New(name)
 		}
 
-		if name != tmpl.Name() {
-			tmpl = tmpl.New(name)
-		}
-
-		if _, err = tmpl.Parse(string(contents)); err != nil {
-			return tmpl, err
+		if _, err = t.tmpl.Parse(string(contents)); err != nil {
+			return err
 		}
 	}
 
-	return tmpl, nil
+	return nil
 }
 
-func (t *BinTemplate) MustLoadDirectory(directory string) *template.Template {
-	if tmpl, err := t.LoadDirectory(directory); err != nil {
+func (t *BinTemplate) MustLoadDirectory(directory string) {
+	if err := t.LoadDirectory(directory); err != nil {
 		panic(err)
-	} else {
-		return tmpl
 	}
 }
